b24c8978df4a29c91508611812c619a4c6809b6a
Roesha Nigos <roeshanigos@fb.com>
2018-07-06 10:37:06 -0700

    Initial Commit

---
+++ socialImpactApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  socialImpactApp
+//
+//  Created by Roesha Nigos on 7/6/18.
+//  Copyright © 2018 teamMorgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ socialImpactApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  socialImpactApp
+//
+//  Created by Roesha Nigos on 7/6/18.
+//  Copyright © 2018 teamMorgan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
