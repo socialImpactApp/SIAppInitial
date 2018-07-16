8bb6b8738276a3418bf64cd9a8a855ddeec8c64b
Roesha Nigos <roeshanigos@fb.com>
2018-07-15 21:02:31 -0700

    added storyboard

---
+++ LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  socialImpactApp
+//
+//  Created by Roesha Nigos on 7/13/18.
+//  Copyright © 2018 teamMorgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
