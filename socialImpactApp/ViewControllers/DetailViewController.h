8c523093a41b438a2ae1ac46089f02d8ec38b928
Roesha Nigos <roeshanigos@fb.com>
2018-07-15 21:27:29 -0700

    added vc's

---
+++ socialImpactApp/ViewControllers/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  socialImpactApp
+//
+//  Created by Roesha Nigos on 7/15/18.
+//  Copyright © 2018 teamMorgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ socialImpactApp/ViewControllers/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  socialImpactApp
+//
+//  Created by Roesha Nigos on 7/15/18.
+//  Copyright © 2018 teamMorgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
